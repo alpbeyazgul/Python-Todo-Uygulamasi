742969a86f92b813bcc90476a1a5c51339bba52a
Alp Beyazgül <alpbeyazgul@hotmail.com>
2023-05-31 00:51:10 +0300

    Create views.py

---
+++ todo_list/todo/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from .forms import TaskForm
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list
