168c7b8623c7e94c4fdaf633cfb726ccb1b9eeae
Alp Beyazgül <alpbeyazgul@hotmail.com>
2023-05-31 00:50:58 +0300

    Create urls.py

---
+++ todo_list/todo/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.task_list, name='task_list'),
+    path('create/', views.create_task, name='create_task'),
+    path('update/<int:pk>/', views.update_task, name='update_task'),
+]
